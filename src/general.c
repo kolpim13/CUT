2ded179bed4f26606c7a73b0404affe21954939f
kolpim13 <torgunakovmaks2@gmail.com>
2022-04-03 21:21:56 +0300

    Queue struct added
    Test version of queue struct added

---
+++ src/general.c
@@ -0,0 +1,52 @@
+#include "general.h"
+
+Queue* Queue_new(size_t length) {
+	Queue* q = (Queue*)malloc(sizeof(Queue));
+	q->data = (void**)malloc(length * sizeof(void*));
+	q->length = length;
+	q->amount = 0;
+	q->pos = 0;
+	return q;
+}
+void Queue_free(Queue* q) {
+	free(q->data);
+	free(q);
+}
+
+bool Queue_add(Queue* q, void* elem) {
+	if (Queue_isFull(q) == true) {
+		return false;
+	}
+
+	q->data[pos] = elem;
+	amount++;
+
+	return true;
+}
+bool Queue_getLast(Queue* q, void* elem) {
+	if (Queue_isEmpty(q) == true) {
+		return false;
+	}
+
+	elem = q->data[pos];
+	return true;
+}
+bool Queue_pop(Queue* q, void* elem) {
+	if (Queue_isEmpty(q) == true) {
+		return false;
+	}
+
+	elem = q->data[q->pos];
+	q->amount--;
+	q->pos++;
+	if (q->pos >= q->length) {
+		q->pos = 0;
+	}
+	return true;
+}
+bool Queue_isFull(Queue* q) {
+	return (q->amount == q->length - 1) ? true : false;
+}
+bool Queue_isEmpty(Queue* q) {
+	return (q->amount == 0) ? true : false;
+}
---
+++ src/inc/general.h
@@ -0,0 +1,26 @@
+#ifndef _GENERAL_H
+#define _GENERAL_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// QUEUE
+typedef struct _Queue {
+	void** data;
+	size_t length;
+	size_t amount;
+	size_t pos;
+}Queue;
+
+Queue* Queue_new(size_t length);
+void Queue_free(Queue* q);
+
+bool Queue_add(Queue* q, void* elem);
+bool Queue_getLast(Queue* q, void* elem);
+bool Queue_pop(Queue* q, void* elem);
+bool Queue_isFull(Queue* q);
+bool Queue_isEmpty(Queue* q);
+//-----------------------------------------------
+
+#endif // !_GENERAL_H
